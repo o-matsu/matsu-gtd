1ed3a7b9f6dbc2867dc87c64c78bdf6a448555f9
o-matsu <matsushita.ryosuke@pine-app.studio>
2025-02-14 19:59:38 +0900

    feat: Add Riverpod and Firebase Authentication repository

---
+++ lib/core/firebase_auth_repository.dart
@@ -0,0 +1,50 @@
+// REF: https://medium.com/@jakob.prossinger/flutter-firebase-authentication-with-riverpod-2-5-and-gorouter-0311ad23550b
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'firebase_auth_repository.g.dart';
+
+class FirebaseAuthenticationRepository {
+  FirebaseAuthenticationRepository(this._firebaseAuth);
+
+  final FirebaseAuth _firebaseAuth;
+  // .. add other functions here
+  User? get currentUser => _firebaseAuth.currentUser;
+  Stream<User?> authStateChanges() {
+    return _firebaseAuth.authStateChanges();
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    return await _firebaseAuth.signInWithCredential(credential);
+  }
+
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
+}
+
+@Riverpod(keepAlive: true)
+FirebaseAuthenticationRepository authRepository(Ref ref) {
+  final auth = ref.watch(firebaseAuthProvider);
+  return FirebaseAuthenticationRepository(auth);
+}
+
+@Riverpod(keepAlive: true)
+FirebaseAuth firebaseAuth(Ref ref) {
+  return FirebaseAuth.instance;
+}
+
+@Riverpod(keepAlive: true)
+Stream<User?> authStateChange(Ref ref) {
+  final auth = ref.watch(authRepositoryProvider);
+  return auth.authStateChanges();
+}
---
+++ lib/core/firebase_auth_repository.g.dart
@@ -0,0 +1,61 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_auth_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authRepositoryHash() => r'222fa4d2b5f40f5c9e90c205ccb564533e9e3263';
+
+/// See also [authRepository].
+@ProviderFor(authRepository)
+final authRepositoryProvider =
+    Provider<FirebaseAuthenticationRepository>.internal(
+  authRepository,
+  name: r'authRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AuthRepositoryRef = ProviderRef<FirebaseAuthenticationRepository>;
+String _$firebaseAuthHash() => r'cb440927c3ab863427fd4b052a8ccba4c024c863';
+
+/// See also [firebaseAuth].
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = Provider<FirebaseAuth>.internal(
+  firebaseAuth,
+  name: r'firebaseAuthProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firebaseAuthHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseAuthRef = ProviderRef<FirebaseAuth>;
+String _$authStateChangeHash() => r'31d8de3c0f9c8352f1024c8cdb74287b5ad88d37';
+
+/// See also [authStateChange].
+@ProviderFor(authStateChange)
+final authStateChangeProvider = StreamProvider<User?>.internal(
+  authStateChange,
+  name: r'authStateChangeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authStateChangeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AuthStateChangeRef = StreamProviderRef<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
