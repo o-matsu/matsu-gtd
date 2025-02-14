2c1b3c361fc43ee4aa622858942ebf882a7410c7
o-matsu <matsushita.ryosuke@pine-app.studio>
2025-02-14 20:04:31 +0900

    chore: Add authentication guard TODO comment for Firebase login

--- lib/router.dart
+++ lib/router.dart
@@ -21,3 +21,8 @@ final router = GoRouter(
   routes: $appRoutes,
   initialLocation: '/inbox',
 );
+
+/// TODO: 認証ガードの実装
+/// 認証してなければ、匿名ユーザーとしてログイン
+/// https://zenn.dev/joo_hashi/articles/9e2eb6fdaf5d1b
+/// https://medium.com/@jakob.prossinger/flutter-firebase-authentication-with-riverpod-2-5-and-gorouter-0311ad23550b
