cb32025a05b4b4c084226461c29e27e4a0bfbd6f
o-matsu <matsushita.ryosuke@pine-app.studio>
2025-02-17 19:55:32 +0900

    refactor: Update MyApp constructor syntax to use super.key

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   static FirebaseAnalyticsObserver observer =
